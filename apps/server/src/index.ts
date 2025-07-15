e94c3c33abda222d93da925a9e942161328c4cea
Pujon Das Auvi <auvipujondas@gmail.com>
2025-07-15 13:09:08 +0600

    feat: Implement server initialization with error handling using only node

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("hi")
+import http from 'http';
+
+const init = async () =>{
+const httpServer = await http.createServer()
+const port = process.env.PORT || 6000;
+    httpServer.listen(port, () => {
+        console.log(`Server is running on port ${port}::: ${new Date().toLocaleString()} \n :::: http://localhost:${port}`);
+    });
+}
+
+init().catch((error) => {
+    console.error('Error starting the server:', error);
+    process.exit(1);
+});
+export default init;
